97110688a94b7e74fc5711d1d4d6b41545c526c1
Aadil Mehraj <aadilmehraj76@gmail.com>
2021-08-07 19:43:11 +0530

    Setup cors

--- pages/api/suggestions.js
+++ pages/api/suggestions.js
@@ -1,7 +1,17 @@
+import Cors from 'cors';
+import { initMiddleware } from '@utils/middlewares';
 import { fetchUnsplashSearchSuggestions } from '@services/unsplash-api';
+
+const cors = initMiddleware(
+  Cors({
+    origin: process.env.DOMAIN_URL,
+    methods: ['GET'],
+  }),
+);
 
 export default async function handler(req, res) {
   try {
+    await cors(req, res);
     const suggestions = await fetchUnsplashSearchSuggestions();
     res.status(200).json(suggestions);
   } catch (error) {
